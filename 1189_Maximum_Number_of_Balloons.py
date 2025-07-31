6699b43d182d21c2cc8cf0f812c012465444a651
AliAoun <20101001-038@uskt.edu.pk>
2025-07-31 23:36:21 +0500

    added leetcode sol 1189

---
+++ 1189_Maximum_Number_of_Balloons.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        countText = Counter(text)
+        countBalloon = Counter("balloon")
+
+        res = len(text)
+        
+        for c in countBalloon:
+            res = min(res, countText[c] // countBalloon[c])
+        return res
+    
+    # time complexity: O(n)
+    # space complexity: O(1)
