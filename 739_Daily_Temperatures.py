ddc8ce45f005128a90305d78fe3d9005546b1d0b
AliAoun <20101001-038@uskt.edu.pk>
2025-08-08 15:10:59 +0500

    added leetcode sol 739

--- 739_Daily_Temperatures.py
+++ 739_Daily_Temperatures.py
@@ -1,17 +1,22 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # res = [0] * len(temperatures)
+        # stack = []
+
+        # for i, t in enumerate(temperatures):
+        #     while stack and t > stack[-1][0]:
+        #         stackT, stackInd = stack.pop()
+        #         res[stackInd] = (i - stackInd)
+        #     stack.append([t, i])
+        # return res 
         res = [0] * len(temperatures)
         stack = []
+        stack.append(0)
 
-        for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                res[stackInd] = (i - stackInd)
-            stack.append([t, i])
-        return res 
-    
-    # time complexity: O(n) where n is the length of temperatures
-    # space complexity: O(n) for the stack used to store temperatures and their indices
+        for i in range(1,len(temperatures)):
+            while stack and temperatures [i]> temperatures[stack[-1]]:
+                prev=stack.pop()
+                res[prev]=i-prev
+            stack.append(i)
 
-
-    
+        return res
