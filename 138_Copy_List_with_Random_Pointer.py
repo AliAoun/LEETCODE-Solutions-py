4ac7642ad3de44bd2478abd32b4514caa4428c7e
AliAoun <20101001-038@uskt.edu.pk>
2025-09-30 10:44:50 +0500

    added leetcode sol 138

---
+++ 138_Copy_List_with_Random_Pointer.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldToCopy = { None : None }
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
+        
+        return oldToCopy[head]
+    
+    # Time Complexity: O(n) where n is the number of nodes in the linked list and we traverse the list twice.
+    # Space Complexity: O(n) where n is the number of nodes in the linked list and we use a hash map to store the mapping of original nodes to their copies.
