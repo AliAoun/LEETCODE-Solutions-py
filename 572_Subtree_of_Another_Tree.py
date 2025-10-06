ad630c4dcf7940a55e0289e536fe5f7d1f550050
AliAoun <20101001-038@uskt.edu.pk>
2025-10-06 11:36:09 +0500

    added leetcode sol 572

---
+++ 572_Subtree_of_Another_Tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        if self.sameTree(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
+        # Checking same tree
+    def sameTree(self, root, subRoot):
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return (self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right))
+        return False
+    
+    # Time Complexity: O(N * M) in worst case where N and M are number of nodes in root and subRoot respectively.
+    # Space Complexity: O(H) where H is the height of the tree. In worst case, H can be N in case of skewed tree.
